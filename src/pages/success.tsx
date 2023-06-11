672eaa463ceb9afdf256fa235068410e5836522b
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2023-06-11 18:56:25 -0300

    feat: initial commit

---
+++ src/pages/success.tsx
@@ -0,0 +1,73 @@
+import { stripe } from '@/lib/stripe';
+import { ImageContainer } from '@/styles/pages/product';
+import { SuccessContainer } from '@/styles/pages/success';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import Stripe from 'stripe';
+
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
+  return (
+    <>
+      <Head>
+        <title>Order placed | Ignite Shop</title>
+
+        <meta name="robots" content="noindex" />
+      </Head>
+      <SuccessContainer>
+        <h1>Order placed successfully!</h1>
+
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt="" />
+        </ImageContainer>
+
+        <p>
+          Sweet! <strong>{customerName}</strong>, your{' '}
+          <strong>{product.name}</strong> is on the way
+        </p>
+
+        <Link href="/">Back to catalog</Link>
+      </SuccessContainer>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  });
+
+  const customerName = session.customer_details?.name;
+  // @ts-ignore
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
